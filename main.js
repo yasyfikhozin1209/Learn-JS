0670033912d5a7ce45a7d85ce82f91e7d81d9cdf
YASYFI KHOZIN <yasyfikhozin1@gmail.com>
2025-06-13 15:55:20 +0700

    update navbar

---
+++ main.js
@@ -0,0 +1,16 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function() {
+    nav.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+});
+
+// Menutup menu saat mengklik link
+const navLinks = document.querySelectorAll('nav ul li a');
+navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        nav.classList.remove('active');
+        menuToggle.classList.remove('active');
+    });
+});
